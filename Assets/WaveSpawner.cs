97ba001221d5c8b213170108dea7d04f4ede4697
awatkins14 <awatkins1@ycp.edu>
2025-07-17 14:29:58 -0400

    tower
    tower

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    private int waveIndex = 0;
+    private bool isSpawning = false;
+
+    /// <summary>
+    /// Called when the Start Round button is clicked
+    /// </summary>
+    public void StartNextWave()
+    {
+        if (!isSpawning)
+            StartCoroutine(SpawnWave());
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        isSpawning = true;
+
+        Debug.Log("Wave incoming!");
+
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        isSpawning = false;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
